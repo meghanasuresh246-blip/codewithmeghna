5c87e0834aeb5c36aa4a445ea8c8e977bfb3b694
meghanasuresh246-blip <meghanasuresh246@gmail.com>
2025-10-31 16:18:03 +0530

    Add files via upload

---
+++ example.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("SIR M V GOVT SCIENCE COLLEGE");
+    getch();
+    return 0;
+}
---
+++ first program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() { 
+    int a, b, sum;
+    printf("enter two numbers:");
+    scarf("%d %d" , &a, &b);
+    
+    sum = a + b;
+    
+    printf("sum = %d\n" , sum);
+    
+    return 0;
+}
+    
